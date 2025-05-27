37b4db9ecce08035faafd7b82a3cf30d75d2ddd4
HolyG <gggigi111@gmail.com>
2025-05-27 19:29:19 +0200

    Edit-Hero_Detail::signal+computed

--- Anguler/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ Anguler/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,24 @@
-import { Hero } from '../hero';
-import { Component, input } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
-  imports: [ CommonModule, FormsModule ],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.scss'
 })
 export class HeroDetailComponent {
   hero = input<Hero>();
+  
+  heroName = computed(() => this.hero()?.name?.toUpperCase() || '');
+  
+  onHeroNameChange(newName: string): void {
+    const currentHero = this.hero();
+    if (currentHero) {
+      currentHero.name = newName;
+    }
+  }
 }
